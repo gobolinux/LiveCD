2693e49463cc2739bcb1e8bdfad82e19c212c184
Lucas Correia Villa Real <lucasvr@gobolinux.org>
2014-01-29 15:58:10 +0000

    Include support for es_MX. Contributed by Mario Alberto Garcia Alonso.

--- Data/Language/LiveCD.pro
+++ Data/Language/LiveCD.pro
@@ -1,3 +1,3 @@
 SOURCES = .Temp/ConfigureLiveCD .Temp/KeymapDialog
 
-TRANSLATIONS = tt2_pt_BR.ts tt2_de_DE.ts tt2_hu_HU.ts
+TRANSLATIONS = tt2_pt_BR.ts tt2_de_DE.ts tt2_hu_HU.ts tt2_es_MX.ts
---
+++ Data/Language/tt2_es_MX.ts
@@ -0,0 +1,36 @@
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="48"/>
+        <source>Keyboard map selection</source>
+        <translation>Selección de distribución de teclado</translation>
+    </message>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="52"/>
+        <source>You may select one of the following keyboard maps.</source>
+        <translation>Puede seleccionar una de las siguientes distribuciones de teclado.</translation>
+    </message>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="53"/>
+        <source>If you do not select a keyboard map, us.map</source>
+        <translation>Si no selecciona una, us.map</translation>
+    </message>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="54"/>
+        <source>(the US keyboard map) is the default.  Use the UP/DOWN</source>
+        <translation>(la distribución del teclado para E.U.) será la elegida. Use ARRIBA/ABAJO</translation>
+    </message>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="55"/>
+        <source>arrow keys and PageUp/PageDown to scroll through the</source>
+        <translation>y AvPág/RePág para desplazarse a través de</translation>
+    </message>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="56"/>
+        <source>whole list of choices.</source>
+        <translation>la lista completa de opciones.</translation>
+    </message>
+</context>
+</TS>
