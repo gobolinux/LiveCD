6c9a4c14e3fbe2a75842ef77d64a481f75cbe16b
Lucas C. Villa Real <lucasvr@br.ibm.com>
2015-12-08 23:53:40 -0200

    Translations to fr_FR. Contribution by Jean-Roch HUET (jeanroch.huet@yahoo.fr)

---
+++ Data/Language/tt2_fr_FR.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="48"/>
+        <source>Keyboard map selection</source>
+        <translation type="unfinished">Sélection de la configuration du clavier</translation>
+    </message>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="52"/>
+        <source>You may select one of the following keyboard maps.</source>
+        <translation type="unfinished">Vous pouvez sélectionner l&apos;une des dispositions suivantes.</translation>
+    </message>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="53"/>
+        <source>If you do not select a keyboard map, us.map</source>
+        <translation type="unfinished">Si aucune disposition n&apos;est sélectionnée, fr.map</translation>
+    </message>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="54"/>
+        <source>(the US keyboard map) is the default.  Use the UP/DOWN</source>
+        <translation type="unfinished">(la disposition française) est celle par défaut.</translation>
+    </message>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="55"/>
+        <source>arrow keys and PageUp/PageDown to scroll through the</source>
+        <translation type="unfinished">Utilisez les touches HAUT/BAS et PAGE PRÉCÉDENTE/SUIVANTE</translation>
+    </message>
+    <message>
+        <location filename=".Temp/KeymapDialog" line="56"/>
+        <source>whole list of choices.</source>
+        <translation type="unfinished">pour faire défiler la liste des choix.</translation>
+    </message>
+</context>
+</TS>
